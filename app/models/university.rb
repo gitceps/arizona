2a3c04cbd50ebd8f19e6c8f58ca5b6be6eceb110
JYeon <rr32121@gmail.com>
2016-08-21 21:38:16 +0900

    merge

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,14 +1,8 @@
 class University < ActiveRecord::Base
-    has_many :distribution_major
-    has_many :distribution_minor
-    has_many :department
+ 
         def self.search(search)
           University.where("name LIKE ?", "%#{:search}%") 
-<<<<<<< HEAD
         end        
     
-=======
-        end
->>>>>>> 8e1ef39a693b59fed5005c7412bc6ab8688fab3a
+    
 end
-
