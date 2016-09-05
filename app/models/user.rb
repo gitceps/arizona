c0b28da0d42ef2ec2028a655fe0ed0d838df2d70
최준성 <cjs@estmob.com>
2016-09-05 21:58:53 +0900

    ec2 인스턴스 facebook api 연동 테스트

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,15 @@ class User < ActiveRecord::Base
         user.email = auth.info.email
         user.password = Devise.friendly_token[0,20]
       end
+        # 이 때는 이상하게도 after_create 콜백이 호출되지 않아서 아래와 같은 조치를 했다.
+        user.add_role :user if user.roles.empty?
+        user   # 최종 반환값은 user 객체이어야 한다.
+  end
+  def self.new_with_session(params, session)
+      super.tap do |user|
+        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+          user.email = data["email"] if user.email.blank?
+        end
+      end
   end
 end
