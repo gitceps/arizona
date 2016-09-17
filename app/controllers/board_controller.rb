4bc182773a2707b61468e8ff03a3170c5ca9a65d
최준성 <cjs@estmob.com>
2016-09-17 13:46:40 +0900

    pagination 마무리(페이지당 게시물 5개)

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,18 +1,21 @@
 class BoardController < ApplicationController
+  @@item_per_page = 5
   def list
       @posts = Post.all.order("created_at DESC")
       @post_count = @posts.count
       @page_count = (@post_count.to_f / 1.to_f).ceil
       
+      @pages = Post.paginate(:page => params[:page], :per_page => @@item_per_page).order('id DESC')
+      
   end
   def view_post
       #@posts = Post.find_by_sql("SELECT * FROM posts order by posts.id desc")
-      @posts = Post.all.order("created_at DESC").limit(1)
+      @posts = Post.all.order("created_at DESC")
       
       @post_count = @posts.count
       @page_count = (@post_count.to_f / 1.to_f).ceil
       
-      @pages = Post.paginate(:page => params[:page], :per_page =>5).order('id DESC')
+      @pages = Post.paginate(:page => params[:page], :per_page =>@@item_per_page).order('id DESC')
       
       @p = @posts.find(params[:id])
       @date = @p.created_at.strftime("%Y-%m-%d %H:%M")
@@ -55,13 +58,14 @@ class BoardController < ApplicationController
       end
   end
   def reply_write
+    #AJAX call
     reply = Reply.new
     reply.content = params[:reply_content]
     reply.post_id = params[:id_of_post]
     reply.like = 0
     reply.dislike = 0
     reply.save
-    #AJAX call
+
     redirect_to "/board/view_post/" + params[:id_of_post].to_s
   end
 end
